dac3152465597de0ae08a9320424a205541f536a
Juan Alday <alday@ieee.org>
2024-05-31 10:25:09 +0200

    Added extra tests

--- algorithms/lcp_array.t.cpp
+++ algorithms/lcp_array.t.cpp
@@ -13,4 +13,6 @@ TEST(Algorithms, lcpArray)
 	EXPECT_THAT(lcpArray("aaaaaa"), ElementsAre(0, 1, 2, 3, 4, 5));
 	EXPECT_THAT(lcpArray("AAAAAA$"), ElementsAre(0, 0, 1, 2, 3, 4, 5));
 	EXPECT_THAT(lcpArray("abcabcddd"), ElementsAre(0, 3, 0, 2, 0, 1, 0, 1, 2));
+	EXPECT_THAT(lcpArray("GATAGACA$"), ElementsAre(0, 0, 1, 1, 1, 0, 0, 2, 0));
+	EXPECT_THAT(lcpArray("ABRACADABRA$"), ElementsAre(0, 0, 1, 4, 1, 1, 0, 3, 0, 0, 0, 2));
 }
